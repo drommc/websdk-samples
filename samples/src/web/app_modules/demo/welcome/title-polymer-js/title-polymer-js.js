23dc129baade75d4275c2aed50e605f3b6cd9280
Julio Stanley <code@juliostanley.com>
2016-04-16 15:43:16 -0400

    First samples commit

---
+++ samples/src/web/app_modules/demo/welcome/title-polymer-js/title-polymer-js.js
@@ -0,0 +1,9 @@
+import lib from 'demo/lib';
+
+// This is still work in progress
+// looking for a cleaner implementation
+lib.register(function(){
+  require('./title-polymer-js.htm')
+});
+
+lib.addComponent(``)
